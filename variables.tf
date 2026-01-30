e980d244e18140126abcec3ff04395f23eb15426
mostafals <mostafa.lagnaoui@gmail.com>
2026-01-30 09:36:20 +0100

    Fix Terraform state lock with concurrency and parallelism

--- variables.tf
+++ variables.tf
@@ -47,28 +47,3 @@ variable "allowed_ssh_source" {
   type        = string
   default     = "0.0.0.0/0"
 }
-# vm kinux
-
-variable "vm_name" {
-  type    = string
-  default = "vm-terraformlabb"
-}
-
-variable "vm_size" {
-  type    = string
-  default = "Standard_B1s"
-}
-
-variable "admin_username" {
-  type    = string
-  default = "azureuse4"
-  
-}
-
-variable "admin_password" {
-  description = "Admin password for VM"
-  type        = string
-  default = "TerraformLab@2026="
-  sensitive   = true
-}
-
